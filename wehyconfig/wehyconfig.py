d60fcb6517a0bbad8ac595a57df114ad4f021059
Bill Hughes <bill@weehooey.com>
2023-09-10 15:34:33 -0400

    Added MkDocs and started docstrings.

--- wehyconfig/wehyconfig.py
+++ wehyconfig/wehyconfig.py
@@ -1,8 +1,21 @@
+"""
+A simple configuration file parser.
+"""
 import tomllib
 from pathlib import Path
 
 
-def read_config(config_source: str, section=""):
+def read_config(config_source: str, section="") -> dict:
+    """Read the specified configuration file.
+
+    Args:
+        config_source (str): A path to either a configuration file or
+            a directory containing one or more configuration files.
+        section (str): If the config_source is a single file, you can
+            specify a section within the configuration file.
+    Returns:
+        dict: A Python dictionary representation of the configuration file.
+    """
     config_path = Path(config_source)
     if config_path.is_dir():
         config_files = _get_config_files(config_path)
@@ -16,10 +29,12 @@ def read_config(config_source: str, section=""):
 
 
 def _get_config_files(config_path: Path()):
+    """Private function."""
     return config_path.iterdir()
 
 
 def _read_config_file(config_file: Path(), section=""):
+    """Private function."""
     with open(config_file, "rb") as file:
         config = tomllib.load(file)
     if section:
